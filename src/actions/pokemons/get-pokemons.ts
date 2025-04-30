6ca351b23bac35728b19556401be383e584cb5b8
Jhonny <jhonnycgarcia@gmail.com>
2025-04-29 23:44:41 -0500

    interfaces de pokeapi

--- src/actions/pokemons/get-pokemons.ts
+++ src/actions/pokemons/get-pokemons.ts
@@ -1,10 +1,18 @@
 import { pokeApi } from '../../config/api/pokeApi';
-import { Pokemon } from '../../domain/entities/pokemon';
+import type { Pokemon } from '../../domain/entities/pokemon';
+import type { PokeAPIPaginatedResponse, PokeAPIPokemon } from '../../infrastructure/interfaces/pokeApi.interfaces';
 
-export const getPokemons = async():Promise<Pokemon[]> => {
+export const getPokemons = async(page: number = 0, limit: number = 20):Promise<Pokemon[]> => {
     try {
-        const url = '/pokemon';
-        const  { data } = await pokeApi.get(url);
+        const url = `/pokemon?offset=${page * limit}&limit=${limit}`;
+        const  { data } = await pokeApi.get<PokeAPIPaginatedResponse>(url);
+
+        const pokemosPromises = data.results.map((info) => {
+            return pokeApi.get<PokeAPIPokemon>(info.url);
+        });
+
+        const pokeApiPokemons = await Promise.all(pokemosPromises);
+
         return [];
 
     } catch (error) {
