ef2f87fb47b9cad4703edb586753ef9a6e407f74
Jhonny <jhonnycgarcia@gmail.com>
2025-04-30 21:06:59 -0500

    Pókemons InfiniteScroll

--- src/presentation/screens/home/HomeScreen.tsx
+++ src/presentation/screens/home/HomeScreen.tsx
@@ -1,17 +1,27 @@
-import { useQuery } from '@tanstack/react-query';
+import { useInfiniteQuery } from '@tanstack/react-query';
 import { StyleSheet, View } from 'react-native';
 import { getPokemons } from '../../../actions';
 import { PokemonBallBg, PokemonCard } from '../../components';
 import { FlatList } from 'react-native-gesture-handler';
-import { Text } from 'react-native-paper';
+import { ActivityIndicator, Text } from 'react-native-paper';
 import { globalTheme } from '../../../config/theme/global-theme';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 export const HomeScreen = () => {
     const { top } = useSafeAreaInsets();
-    const { data: pokemons = [], isLoading } = useQuery({
-        queryKey: ['pokemons'],
-        queryFn: () => getPokemons(),
+
+    // esta es la forma tradicional de realizar una peticion HTTP
+    // const { data: pokemons = [], isLoading } = useQuery({
+    //     queryKey: ['pokemons'],
+    //     queryFn: () => getPokemons(),
+    //     staleTime: 1000 * 60 * 60, // 60 minutes
+    // });
+
+    const { data, isLoading, fetchNextPage } = useInfiniteQuery({
+        queryKey: ['pokemons', 'infinite'],
+        initialPageParam: 0,
+        queryFn: ({ pageParam = 0 }) => getPokemons(pageParam),
+        getNextPageParam: (lastPage, pages) => pages.length,
         staleTime: 1000 * 60 * 60, // 60 minutes
     });
 
@@ -21,7 +31,7 @@ export const HomeScreen = () => {
         >
             <PokemonBallBg style={styles.imgPosition} />
             <FlatList
-                data={pokemons}
+                data={data?.pages.flat() ?? []}
                 keyExtractor={(item, index) => `${item.id}-${index}`}
                 numColumns={2}
                 style={{ paddingTop: top + 20 }}
@@ -31,6 +41,12 @@ export const HomeScreen = () => {
                 renderItem={({ item }) => (
                     <PokemonCard pokemon={item} />
                 )}
+                onEndReachedThreshold={ 0.6 }
+                onEndReached={() => fetchNextPage()}
+                showsVerticalScrollIndicator={false}
+                // ListFooterComponent={() => (
+                //     <ActivityIndicator size="large" color={globalTheme.colors.primary} />
+                // )}
             />
         </View>
     );
