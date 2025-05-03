5f148eb663f731810896fc6f7972ce4938845268
Jhonny <jhonnycgarcia@gmail.com>
2025-05-02 21:58:06 -0500

    filtrar listado por id y nombre

--- src/presentation/screens/search/SearchScreen.tsx
+++ src/presentation/screens/search/SearchScreen.tsx
@@ -1,18 +1,34 @@
 import { View, FlatList } from 'react-native';
 import { globalTheme } from '../../../config/theme/global-theme';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
-import { ActivityIndicator, TextInput } from 'react-native-paper';
+import { ActivityIndicator, Text, TextInput } from 'react-native-paper';
 import { Pokemon } from '../../../domain/entities/pokemon';
 import { PokemonCard } from '../../components';
 import { getPokemonNameWithId } from '../../../actions';
 import { useQuery } from '@tanstack/react-query';
+import { useMemo, useState } from 'react';
 
 export const SearchScreen = () => {
     const { top } = useSafeAreaInsets();
+    const [term, setTerm] = useState('');
 
     const { data: pokemonNameList, isLoading } = useQuery({
         queryKey: ['pokemons', 'all'],
         queryFn: () => getPokemonNameWithId(),
+    });
+
+    const pokemonNameIdList = useMemo(() => {
+        // es un numero
+        if(!isNaN(Number(term))) {
+            const pokemon = pokemonNameList?.find((item) => item.id === Number(term));
+            return pokemon ? [pokemon] : [];
+        }
+
+        if(term.length === 0) return [];
+
+        if(term.length < 3) return [];
+
+        return pokemonNameList?.filter((item) => item.name.includes(term.toLowerCase())) ?? [];
     });
 
     return (
@@ -22,11 +38,13 @@ export const SearchScreen = () => {
                 mode="flat"
                 autoFocus
                 autoCorrect={false}
-                onChangeText={() => {}}
-                value={''}
+                onChangeText={setTerm}
+                value={term}
             />
 
             <ActivityIndicator style={{ paddingTop: 20 }} size="large" />
+
+            <Text>{JSON.stringify(pokemonNameIdList, null, 2)}</Text>
 
             <FlatList
                 data={[] as Pokemon[]}
